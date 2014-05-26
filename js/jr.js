330eb9b80bdaa1d97e3f2a5d5a36b5ad4cc78515
priyanka nag <priynag@gmail.com>
2014-05-26 16:09:36 +0530

    Scrollback support room is now embeded into the documentation site

--- js/jr.js
+++ js/jr.js
@@ -2,6 +2,18 @@
  * Master Top Global Root Base Special Parent Object.... thingy
  */
 
+ window.scrollback = {
+  streams:["support"],
+  theme: 'light',
+  ticker: true
+ };
+ 
+ /***** don't edit below *****/
+ (function(d,s,h,e){e=d.createElement(s);e.async=1;
+ e.src=h+'/client.min.js';scrollback.host=h;
+ d.getElementsByTagName(s)[0].parentNode.appendChild(e);}
+ (document,'script',(location.protocol=="https:"?"https:":"http:")+'//scrollback.io'));
+
 var jr = {
 	body : null,
 	markdownContent: null,
