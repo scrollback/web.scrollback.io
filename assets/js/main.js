52b3e7c6b77c4c3b7b69ea7a8e1391f92acc51be
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-11-03 04:25:18 +0530

    Update lace

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,6 @@
 /* global $ */
 
 $(function() {
-    var $createField, $createText, $createInput, $createSubmit, $createError;
-
     // Hide non-javascript content
     $("<style>").attr("type", "text/css").text(".nojs { display: none; }").appendTo("head");
 
@@ -12,98 +10,73 @@ $(function() {
         $("body").addClass("scrollback-open");
     });
 
-    // Show and hide the modal dialog
-    function showDialog() {
-        if (history.pushState) {
-            history.pushState(null, "");
+    // Show the modal dialog
+    $(".login-show").on("click", function() {
+        var $dialog, $createField, $createText, $createInput, $createSubmit, $createError;
+
+        function error(err) {
+            $createError.text(err);
+
+            if (typeof err === "undefined") {
+                $createField.attr("disabled", false);
+
+                return true;
+            } else {
+                $createField.attr("disabled", true);
+
+                return false;
+            }
         }
 
-        $("body").addClass("login-open");
-        $createInput.focus();
-    }
+        function validate() {
+            var name = $(this).val();
 
-    function hideDialog() {
-        $("body").addClass("login-intermediate");
+            if (name.length  === 0) {
+                return error("");
+            }
 
-        setTimeout(function() {
-            $("body").removeClass("login-open login-intermediate");
-        }, 500);
-    }
+            if (name.length > 0 && name.length < 3) {
+                return error("Room name must be at least 3 letters long.");
+            }
 
-    function handleKey(e) {
-        if (e.keyCode === 27) {
-            hideDialog();
+            if (/^[^a-z]/.test(name)) {
+                return error("Room name must start with a lower case letter.");
+            }
+
+            if (/[^0-9a-z\-]/.test(name)) {
+                return error("Room name must have only lowercase letters, digits and hyphens (-)");
+            }
+
+            if (name.length >= 3) {
+                return error("");
+            }
+
+            return error();
         }
-    }
 
-    $(window).on("keyup", handleKey);
-    $(window).on("popstate", hideDialog);
+        $dialog = $("<div>").html(
+            $("#goto-template").html()
+        ).modal();
 
-    $(".login-show").on("click", showDialog);
-    $(".login-close").on("click", hideDialog);
-    $(".dim").on("click", hideDialog);
+        // Validate form submission
+        $createField = $dialog.find("#create-field");
+        $createText = $createField.find("#create-text");
+        $createInput = $createField.find("input[type=submit]");
+        $createError = $createField.find(".error");
 
-    // Validate form submission
-    $createField = $("#create-field");
-    $createText = $("#create-text");
-    $createInput = $createField.find("input[type=submit]");
-    $createError = $createField.find(".error");
+        // Prevent form submission if input not valid
+        $createText.on("focus keyup change", validate);
 
-    function error(err) {
-        $createError.text(err);
+        $createField.on("submit", function(e) {
+            location.href = "https://scrollback.io/" + $createText.val();
 
-        if (typeof err === "undefined") {
-            $createField.attr("disabled", false);
+            $createText.val("");
 
-            return true;
-        } else {
-            $createField.attr("disabled", true);
+            $(this).attr("disabled", true);
+
+            e.preventDefault();
 
             return false;
-        }
-    }
-
-    function validate() {
-        var name = $createText.val();
-
-        if (name.length  === 0) {
-            return error("");
-        }
-
-        if (name.length > 0 && name.length < 3) {
-            return error("Room name must be at least 3 letters long.");
-        }
-
-        if (/^[^a-z]/.test(name)) {
-            return error("Room name must start with a lower case letter.");
-        }
-
-        if (/[^0-9a-z\-]/.test(name)) {
-            return error("Room name must have only lowercase letters, digits and hyphens (-)");
-        }
-
-        if (name.length >= 3) {
-            return error("");
-        }
-
-        return error();
-    }
-
-    // Handle submit button click
-    $createSubmit.click(function() {
-        location.href = "https://scrollback.io/" + $createText.val();
-
-        $createText.val("");
-
-        $(this).attr("disabled", true);
-    });
-
-    // Prevent form submission if input not valid
-    $createText.on("focus keyup change", validate);
-
-    $createSubmit.submit(function(e) {
-        e.preventDefault();
-
-        return false;
+        });
     });
 });
